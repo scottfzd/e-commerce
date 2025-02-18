be4f96caa33e366c7096840abcc296cd1aa2fa8a
scottfzd <john.fitzgerald@epitech.eu>
2025-02-18 15:03:12 +0100

    add repository to get data

---
+++ app/lib/features/carts/domain/repositories/cart_repository.dart
@@ -0,0 +1,7 @@
+import 'package:app/features/carts/domain/entities/cart_entity.dart';
+import 'package:dartz/dartz.dart';
+import 'package:app/core/error/failures.dart';
+
+abstract class CartRepository {
+  Future<Either<Failure, CartEntity>>getCartById(int cartId);
+}
