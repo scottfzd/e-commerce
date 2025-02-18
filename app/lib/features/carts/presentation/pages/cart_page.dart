ff0b38b5234567ba0f4ebc08806d2b5db4ab0b20
scottfzd <john.fitzgerald@epitech.eu>
2025-02-18 14:59:35 +0100

    feat: fetch cart in initState

--- app/lib/features/carts/presentation/pages/cart_page.dart
+++ app/lib/features/carts/presentation/pages/cart_page.dart
@@ -1,10 +1,37 @@
+import 'package:app/features/carts/domain/repositories/cart_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:app/service_locator.dart';
 
-class CartPage extends StatelessWidget {
+
+class CartPage extends StatefulWidget {
   const CartPage({super.key});
 
   @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    print('I AM IN');
+
+    _loadCartData();
+
+  }
+
+  Future<void> _loadCartData() async {
+    final cart = await sl<CartRepository>().getCartById(47);
+    print(cart);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    // return Container(
+      return const Scaffold(
+        body: Text('HELLO'),
+      );
+    // );
   }
 }
