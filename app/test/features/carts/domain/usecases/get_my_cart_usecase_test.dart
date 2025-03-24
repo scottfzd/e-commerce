df78a73ae58e7fe53cb5e4c364b41836c139d05f
scottfzd <john.fitzgerald@epitech.eu>
2025-03-24 10:30:15 +0100

    fix(test): getMyCart returns Either, so test should expect 'const Right(cart)' rather than 'cart'

--- app/test/features/carts/domain/usecases/get_my_cart_usecase_test.dart
+++ app/test/features/carts/domain/usecases/get_my_cart_usecase_test.dart
@@ -1,11 +1,11 @@
 import 'package:app/features/carts/domain/entities/cart_entity.dart';
 import 'package:app/features/carts/domain/repositories/cart_repository.dart';
 import 'package:app/features/carts/domain/usecases/get_my_cart_usecase.dart';
+
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-
 import 'get_my_cart_usecase_test.mocks.dart';
 
 @GenerateMocks([CartRepository])
@@ -14,6 +14,7 @@ void main() {
   late GetMyCartUsecase getMyCartUsecase;
   late MockCartRepository mockCartRepository;
   const cart = CartEntity();
+  const int shopId = 1;
   
   setUp(() {
 
@@ -25,14 +26,14 @@ void main() {
 
   test('should get open cart associated with current shop', () async {
 
-    when(mockCartRepository.getMyCart(1)).thenAnswer((_) async => const Right(cart));
+    when(mockCartRepository.getMyCart(shopId)).thenAnswer((_) async => const Right(cart));
 
-    final result = await getMyCartUsecase(1);
+    final result = await getMyCartUsecase(shopId);
 
 
     // assert
-    expect(result, equals(cart));
-    verify(mockCartRepository.getMyCart(1)).called(1);
+    expect(result, equals(const Right(cart)));
+    verify(mockCartRepository.getMyCart(shopId)).called(1);
 
   });
 }
